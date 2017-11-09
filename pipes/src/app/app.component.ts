3b9f697f5ab96c1b961103708d05ab7c975e8fe8
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-11-09 18:35:31 -0500

    fin pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Nicolas"
+  nombre2 = "nicolaS jeSuS rodriguez NiñoLes"
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10]
+  PI = Math.PI
+  a = 0.234
+  salario = 1234.5
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: "500",
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout( ()=> resolve('llego la data!'), 3500);
+  })
+
+  fecha = new Date();
+
+  activar:boolean = true
+
 }
--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CapPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
