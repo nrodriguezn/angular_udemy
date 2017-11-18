303f21e34ee6e3129618c3f53f02bb51bc4ed9cb
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-11-17 22:17:11 -0500

    avances antes de meterme con rutas hijas

--- miscelaneos/src/app/components/clases/clases.component.ts
+++ miscelaneos/src/app/components/clases/clases.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClasesComponent implements OnInit {
 
   alerta:string = "alert-danger"
+  loading:boolean = false;
 
   propiedades:Object = {
     danger: false
@@ -15,6 +16,11 @@ export class ClasesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ejecutar(){
+    this.loading = true;
+    setTimeout( ()=> this.loading = false, 3000)
   }
 
 }
